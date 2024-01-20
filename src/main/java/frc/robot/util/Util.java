99138f6ed10f07a71202e2b54cbd59df951ed76b
PreedomCL <54563759+PreedomCL@users.noreply.github.com>
2024-01-19 19:23:45 -0700

    Adjust joystick curve

--- src/main/java/frc/robot/util/Util.java
+++ src/main/java/frc/robot/util/Util.java
@@ -3,12 +3,13 @@ package frc.robot.util;
 public class Util {
 
     public static double joystickCurve(double input) {
-        double magnitude = Math.abs(input);
-        if(magnitude < 0.5) {
-            return Math.signum(input) * (Math.pow(magnitude, 3) + .25 * input);
-        }
+        return input;
+        // double magnitude = Math.abs(input);
+        // if(magnitude < 0.5) {
+        //     return Math.signum(input) * (Math.pow(magnitude, 3) + .25 * input);
+        // }
         
-        return Math.signum(input) * (-1 * Math.pow(magnitude, 3) + 3 * Math.pow(magnitude, 2) + .25);
+        // return Math.signum(input) * (-1 * Math.pow(magnitude, 3) + 3 * Math.pow(magnitude, 2) - 1.25 * magnitude + .25);
     }
 
 }
