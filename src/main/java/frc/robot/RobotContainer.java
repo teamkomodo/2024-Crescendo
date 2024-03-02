cfef7bc0c4234d185014ec6b376cfaf3c6450838
joshnageli <joshuanageli@gmail.com>
2024-03-01 17:53:46 -0700

    fixed auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -42,6 +43,9 @@ public class RobotContainer {
     public RobotContainer() {
         configureBindings();
         autoChooser = AutoBuilder.buildAutoChooser();
+
+
+        SmartDashboard.putData("Auto Chooser",autoChooser);
     }
 
     private String armToSpeaker;
