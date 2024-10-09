3586ddb32e8df9d409d40614b49909fda582d799
joshnageli <joshuanageli@gmail.com>
2024-10-09 16:47:14 -0600

    fixed constants to import over to main

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,25 +42,25 @@ public final class Constants {
     public static final double DRIVETRAIN_WIDTH = 0.5271; // Distance between center of left and right swerve wheels in meters
     public static final double DRIVETRAIN_LENGTH = 0.5271; // Distance between center of front and back swerve wheels in meters
 
-    public static final int FRONT_LEFT_DRIVE_MOTOR_ID = 16;
-    public static final int FRONT_LEFT_STEER_MOTOR_ID = 17;
-    public static final int FRONT_LEFT_STEER_ENCODER_ID = 23;
-    public static final double FRONT_LEFT_STEER_OFFSET = -Math.toRadians(24.697);
+    public static final int FRONT_LEFT_DRIVE_MOTOR_ID = 10;
+    public static final int FRONT_LEFT_STEER_MOTOR_ID = 11;
+    public static final int FRONT_LEFT_STEER_ENCODER_ID = 20;
+    public static final double FRONT_LEFT_STEER_OFFSET = 4.71;
 
     public static final int FRONT_RIGHT_DRIVE_MOTOR_ID = 12;
-    public static final int FRONT_RIGHT_STEER_MOTOR_ID = 13;
-    public static final int FRONT_RIGHT_STEER_ENCODER_ID = 21;
-    public static final double FRONT_RIGHT_STEER_OFFSET = -Math.toRadians(-72.510);
+    public static final int FRONT_RIGHT_STEER_MOTOR_ID = 15;
+    public static final int FRONT_RIGHT_STEER_ENCODER_ID = 22;
+    public static final double FRONT_RIGHT_STEER_OFFSET = 3.75;
 
     public static final int BACK_LEFT_DRIVE_MOTOR_ID = 14;
-    public static final int BACK_LEFT_STEER_MOTOR_ID = 15;
-    public static final int BACK_LEFT_STEER_ENCODER_ID = 22;
-    public static final double BACK_LEFT_STEER_OFFSET = -Math.toRadians(174.841);
+    public static final int BACK_LEFT_STEER_MOTOR_ID = 13;
+    public static final int BACK_LEFT_STEER_ENCODER_ID = 21;
+    public static final double BACK_LEFT_STEER_OFFSET = 1.28;
 
-    public static final int BACK_RIGHT_DRIVE_MOTOR_ID = 10;
-    public static final int BACK_RIGHT_STEER_MOTOR_ID = 11;
-    public static final int BACK_RIGHT_STEER_ENCODER_ID = 20;
-    public static final double BACK_RIGHT_STEER_OFFSET = -Math.toRadians(57.920);
+    public static final int BACK_RIGHT_DRIVE_MOTOR_ID = 16;
+    public static final int BACK_RIGHT_STEER_MOTOR_ID = 17;
+    public static final int BACK_RIGHT_STEER_ENCODER_ID = 23;
+    public static final double BACK_RIGHT_STEER_OFFSET = 5.9;
 
     public static final double WHEEL_DIAMETER = 0.1016;
 
